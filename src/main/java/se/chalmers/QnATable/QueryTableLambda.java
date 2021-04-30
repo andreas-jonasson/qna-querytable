2a07e9f5e9eeabd79392ba79911fb5eca08244e3
andreas <andreas@chalmers.se>
2021-04-30 12:42:08 +0200

    Started with the communication with DynamoDB in QueryTableLambda.

--- src/main/java/se/chalmers/QnATable/QueryTableLambda.java
+++ src/main/java/se/chalmers/QnATable/QueryTableLambda.java
@@ -1,5 +1,11 @@
 package se.chalmers.QnATable;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.GetItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -7,6 +13,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import se.chalmers.qna.fulfillment.model.QNABotFullfillmentRequest;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,13 +25,19 @@ public class QueryTableLambda implements RequestHandler<QNABotFullfillmentReques
     private static final String QNA_LIST_PRIMARY_KEY = System.getenv("QNA_LIST_PRIMARY_KEY");
     private static final String QNA_LIST_SORT_KEY =  System.getenv("QNA_LIST_SORT_KEY");
     private static final String QNA_LIST_VALUE_KEY =  System.getenv("QNA_LIST_VALUE_KEY");
-    private Set<TableRow> rows = new HashSet<>();
-
+    private static final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
 
     @Override
     public String handleRequest(QNABotFullfillmentRequest event, Context context)
     {
-        if (event == null && context == null) {
+        Set<TableRow> rows = new HashSet<>();
+        String topic;
+        String category;
+        GetItemRequest request;
+
+        if (event == null && context == null)
+        {
+
             rows = UpdateTableLambda.parseLocalFile();
             for (TableRow row : rows)
                 System.out.println(row);
@@ -35,6 +48,42 @@ public class QueryTableLambda implements RequestHandler<QNABotFullfillmentReques
         logger.log("Inside SeedDynamoDB::handleRequest");
         logger.log("Event:\n" + gson.toJson(event));
 
+        topic = getTopic(event);
+        topic = "fruit";
+
+        category = getCategory(event);
+        category = "yellow";
+
+        request = createItemRequest(topic, category);
+
         return "Done.";
     }
+
+    private GetItemRequest createItemRequest(String primaryKey, String sortKey)
+    {
+        return new GetItemRequest();
+    }
+
+    private String getTopic(QNABotFullfillmentRequest event)
+    {
+        return "";
+    }
+
+    private String getCategory(QNABotFullfillmentRequest event)
+    {
+        return "";
+    }
+
+    private Set<TableRow> getList(String topic, String category)
+    {
+        Set<TableRow> result = new HashSet<>();
+
+        HashMap<String,AttributeValue> key_to_get =
+                new HashMap<String,AttributeValue>();
+
+        //key_to_get.put(QNA_LIST_TABLE_NAME, new AttributeValue(name));
+
+        return result;
+    }
+
 }
